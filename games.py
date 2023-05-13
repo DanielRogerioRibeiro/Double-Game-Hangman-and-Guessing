d329d9307c2ab012b71dee4b13683dad3e740cad
DanielRogerioRibeiro <danielrogerioribeiro.dev@gmail.com>
2023-05-13 08:30:58 -0300

    Including the double game

---
+++ games.py
@@ -0,0 +1,11 @@
+import forca
+import adivinhacao
+
+def escolhe_jogo():
+    print("*********************************")
+    print("*******Escolha o seu jogo!*******")
+    print("*********************************")
+
+    print("(1) Forca (2) Adivinhação")
+
+    #Including the double game

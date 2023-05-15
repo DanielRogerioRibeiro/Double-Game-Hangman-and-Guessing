a6eb1347a8f3eadb146d559670a66e3dae201770
DanielRogerioRibeiro <danielrogerioribeiro.dev@gmail.com>
2023-05-14 22:18:13 -0300

    Defining which game the user wants to play.

--- games.py
+++ games.py
@@ -8,4 +8,14 @@ def escolhe_jogo():
 
     print("(1) Forca (2) Adivinhação")
 
-    #Including the double game
+    jogo = int(input("Qual jogo? "))
+
+    if(jogo == 1):
+        print("Jogando forca")
+        forca.jogar()
+    elif(jogo == 2):
+        print("Jogando adivinhação")
+        adivinhacao.jogar()
+
+if(__name__ == "__main__"):
+    escolhe_jogo()

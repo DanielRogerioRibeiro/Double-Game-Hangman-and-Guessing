9507692cff24ef49e20284fd02a31ac472e87cea
DanielRogerioRibeiro <danielrogerioribeiro.dev@gmail.com>
2023-05-16 22:40:30 -0300

    Inserting the functions to call the games.

--- divination.py
+++ divination.py
@@ -4,63 +4,68 @@
 
 import random
 
+
+def jogar():
 # Apresentação
-print ("***** Olá seja bem vindo ao Jogo da Adivinhação*****")
-print ("***** Você deverá acertar a informação secreta*****")
+    print ("***** Olá seja bem vindo ao Jogo da Adivinhação*****")
+    print ("***** Você deverá acertar a informação secreta*****")
 
 
-# Resposta da Informação Secreta
-informacao_secreta = random.randrange(1,51)
-#print ("Informação secreta é: ", informacao_secreta)
+    # Resposta da Informação Secreta
+    informacao_secreta = random.randrange(1,51)
+    #print ("Informação secreta é: ", informacao_secreta)
 
-# Variáveis Globais
-total_de_tentativas = 0
-score               = 500
+    # Variáveis Globais
+    total_de_tentativas = 0
+    score               = 500
 
-#Selecionando o nível
-print("Qual o nível de dificuldade que você deseja tentar?")
-print("(1) Fácil (2) Moderado (3) Difícil")
-nivel = int(input("Defina o seu nível: "))
+    #Selecionando o nível
+    print("Qual o nível de dificuldade que você deseja tentar?")
+    print("(1) Fácil (2) Moderado (3) Difícil")
+    nivel = int(input("Defina o seu nível: "))
 
-if(nivel == 1): 
-    total_de_tentativas = 15
-elif(nivel == 2):
-    total_de_tentativas = 10
-else:
-    total_de_tentativas = 5
-
-# Realizando Loop
-for rodada in range (1, total_de_tentativas + 1):
-    print ("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    tentativa = input("Digite seu palpite entre 1 e 50: ")
-    resultado = int(tentativa)
-
-    if (resultado < 1 or resultado > 50):
-        print("Você deve digitar um valor entre 1 e 50")
-        continue
-
-# Variáveis
-    acertou         = resultado == informacao_secreta
-    chute_for_maior = resultado > informacao_secreta
-    chute_for_menor = resultado < informacao_secreta
-    
-            
-    if (acertou):
-        print ("Parabéns você acertou")
-        print ("Sua pontuação foi de {} pontos.".format(pontos))
-        break
-
+    if(nivel == 1): 
+        total_de_tentativas = 15
+    elif(nivel == 2):
+        total_de_tentativas = 10
     else:
-       if (chute_for_maior):
-            print ("Você Errou. Seu palpite foi maior")
-       elif (chute_for_menor):
-            print ("Você Errou. Seu palpite foi menor")
-       pontos_perdidos = abs(informacao_secreta - resultado)
-       pontos          = score - pontos_perdidos
-   
-    print ("Não foi desta vez....")
+        total_de_tentativas = 5
+
+    # Realizando Loop
+    for rodada in range (1, total_de_tentativas + 1):
+        print ("Tentativa {} de {}".format(rodada, total_de_tentativas))
+        tentativa = input("Digite seu palpite entre 1 e 50: ")
+        resultado = int(tentativa)
+
+        if (resultado < 1 or resultado > 50):
+            print("Você deve digitar um valor entre 1 e 50")
+            continue
+
+    # Variáveis
+        acertou         = resultado == informacao_secreta
+        chute_for_maior = resultado > informacao_secreta
+        chute_for_menor = resultado < informacao_secreta
         
-     
-print ("A informação secreta era: ", informacao_secreta)
-print ("Obrigado por ter participado do Jogo")
-print ("*****FIM DE JOGO*****")
+                
+        if (acertou):
+            print ("Parabéns você acertou")
+            print ("Sua pontuação foi de {} pontos.".format(pontos))
+            break
+
+        else:
+            if (chute_for_maior):
+                print ("Você Errou. Seu palpite foi maior")
+            elif (chute_for_menor):
+                print ("Você Errou. Seu palpite foi menor")
+        pontos_perdidos = abs(informacao_secreta - resultado)
+        pontos          = score - pontos_perdidos
+    
+        print ("Não foi desta vez....")
+            
+        
+    print ("A informação secreta era: ", informacao_secreta)
+    print ("Obrigado por ter participado do Jogo")
+    print ("*****FIM DE JOGO*****")
+
+if(__name__ == "__main__"):
+    jogar()
